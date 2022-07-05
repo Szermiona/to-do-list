2d74471acd9fcce7aa276f19d4a22cfb95a68e8c
Shirley Sopotnicka <shirley.sopotnicka@gmail.com>
2022-07-05 11:55:36 +0200

    removed unnecessary annotations

--- src/main/java/com/todolist/TodolistApplication.java
+++ src/main/java/com/todolist/TodolistApplication.java
@@ -2,16 +2,9 @@ package com.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }, scanBasePackages = "com.todolist")
-@EntityScan
-@EnableJpaRepositories
-@EnableConfigurationProperties
+
+@SpringBootApplication
 public class TodolistApplication {
 
 	public static void main(String[] args) {
